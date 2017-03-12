9d43f3defd59c140956da688c17a963a13ba7a19
mattvox <vox-mini@Matthews-Mini.home>
2017-03-12 18:53:55 -0400

    Testing image add.

--- messages.js
+++ messages.js
@@ -1,5 +1,11 @@
 'use strict'
 
+/* global process */
+
+var request = require('request');
+
+var PAGE_ACCESS_TOKEN = process.env.FB_PAGE_ACCESS_TOKEN;
+
 module.exports = {
   sendTextMessage: function (recipientId, messageText) {
       console.log('test', recipientId, messageText);
@@ -14,4 +20,28 @@ module.exports = {
 
       callSendAPI(messageData);
   }
+}
+
+function callSendAPI(messageData) {
+    request({
+        uri: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: PAGE_ACCESS_TOKEN
+        },
+        method: 'POST',
+        json: messageData
+
+    }, function (error, response, body) {
+        console.log('status code', response.statusCode);
+        if (!error && response.statusCode === 200) {
+            var recipientId = body.recipient_id;
+            var messageId = body.message_id;
+
+            console.log("Successfully sent generic message with id %s to recipient %s", messageId, recipientId);
+        } else {
+            console.error("Unable to send message.");
+            // console.error(response);
+            // console.error(error);
+        }
+    });
 }
